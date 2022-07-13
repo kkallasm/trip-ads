e14c8581f802f061dd06e8ae86bdd23240baf73c
kkallasm <kaspar68@gmail.com>
2022-07-13 22:11:39 +0300

    prettier

--- app.ts
+++ app.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import adRouter from './src/routes/ads'
 import cors from 'cors'
-import helmet from "helmet";
-import {connectToDatabase} from "./src/utils/db";
-import {logger} from "@typegoose/typegoose/lib/logSettings";
+import helmet from 'helmet'
+import { connectToDatabase } from './src/utils/db'
+import { logger } from '@typegoose/typegoose/lib/logSettings'
 
 dotenv.config()
 
-const app: Express = express();
+const app: Express = express()
 const port = process.env.PORT
 
 app.use(express.json())
@@ -16,7 +16,7 @@ app.use(
     cors({
         origin: 'http://localhost:3000',
     })
-);
+)
 app.use(helmet())
 
 app.get('/', (req: Request, res: Response) => {
@@ -27,5 +27,5 @@ app.use('/ads', adRouter)
 
 app.listen(port, async () => {
     await connectToDatabase()
-    logger.info(`Server listening at htp://localhost:${port}`);
-});
+    logger.info(`Server listening at htp://localhost:${port}`)
+})
