5e5aa4a461f68371bbcf3c0a6a97a40e2e993b44
kkallasm <kaspar68@gmail.com>
2022-07-13 21:54:30 +0300

    app

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import adRouter from './src/routes/ads'
+import cors from 'cors'
+import helmet from "helmet";
+import {connectToDatabase} from "./src/utils/db";
+import {logger} from "@typegoose/typegoose/lib/logSettings";
 
 dotenv.config()
 
@@ -8,13 +12,20 @@ const app: Express = express();
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    })
+);
+app.use(helmet())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
+    res.send('Trip.ee Ads API')
 })
 
 app.use('/ads', adRouter)
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(port, async () => {
+    await connectToDatabase()
+    logger.info(`Server listening at htp://localhost:${port}`);
 });
---
+++ src/utils/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import logger from "./logger";
+
+export async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string, {
+            dbName: 'ads'
+        })
+        logger.info("Connect to database")
+    } catch (e) {
+        logger.error(e, "Failed to connect to database.")
+        process.exit(1)
+    }
+}
+
+export async function disconnectFromDatabase() {
+    await mongoose.connection.close();
+
+    logger.info("Disconnect from database");
+
+    return;
+}
