6de988a89471ca313d4c49991282728ff57928ea
kkallasm <kaspar68@gmail.com>
2023-09-18 13:30:31 +0300

    get active ads

--- src/modules/ads/ads.service.ts
+++ src/modules/ads/ads.service.ts
@@ -1,6 +1,6 @@
-import { CampaignAd } from "../campaign/campaign.model";
-import { activeAdsResponse, EnumAdLocationType } from "../campaignAd/campaignAd.model";
-import { db } from "../../utils/database";
+import { CampaignAd } from '../campaign/campaign.model'
+import { EnumAdLocationType } from '../campaignAd/campaignAd.model'
+import { db } from '../../utils/database'
 
 export async function getActiveAdsByLocation(location: EnumAdLocationType) {
     const today = new Date().toDateString()
@@ -11,48 +11,58 @@ export async function getActiveAdsByLocation(location: EnumAdLocationType) {
         .where('campaigns.end_date', '>=', today)
         .where('ads.location', '=', location)
         .where('ads.active', '=', true)
-        .selectAll()
-        //.select(['ads.id'])
-        //.select(['ads.id', 'ads.campaign_id', 'ads.image_name', 'ads.location', 'ads.active', 'campaigns.url as url'])
+        .select([
+            'ads.id',
+            'ads.campaign_id',
+            'ads.image_name',
+            'ads.location',
+            'ads.active',
+        ])
         .execute()
 
-    return ads.map(ad => new CampaignAd(ad))
+    return ads.map((ad) => new CampaignAd(ad))
 }
 
-export async function addAdImpression(
-  adId: number,
-  campaignId: number
-) {
+export async function addAdImpression(adId: number, campaignId: number) {
     await db
         .insertInto('stats')
-        .values({ad_id: adId, campaign_id: campaignId, impressions: 1, clicks: 1})
-        .onConflict((oc) => oc
-            .columns(['ad_id', 'campaign_id'])
-            .doUpdateSet({
-                impressions: (eb) => eb('stats.impressions', '+', 1)
-            })
-            .where('stats.ad_id', '=', adId)
-            .where('stats.campaign_id', '=', campaignId)
+        .values({
+            ad_id: adId,
+            campaign_id: campaignId,
+            impressions: 1,
+            clicks: 1,
+        })
+        .onConflict((oc) =>
+            oc
+                .columns(['ad_id', 'campaign_id'])
+                .doUpdateSet({
+                    impressions: (eb) => eb('stats.impressions', '+', 1),
+                })
+                .where('stats.ad_id', '=', adId)
+                .where('stats.campaign_id', '=', campaignId)
         )
-        .execute();
+        .execute()
 }
 
-export async function addAdClick(
-    adId: number,
-    campaignId: number
-) {
+export async function addAdClick(adId: number, campaignId: number) {
     await db
         .insertInto('stats')
-        .values({ad_id: adId, campaign_id: campaignId, impressions: 1, clicks: 1})
-        .onConflict((oc) => oc
-            .columns(['ad_id', 'campaign_id'])
-            .doUpdateSet({
-                clicks: (eb) => eb('stats.clicks', '+', 1)
-            })
-            .where('stats.ad_id', '=', adId)
-            .where('stats.campaign_id', '=', campaignId)
+        .values({
+            ad_id: adId,
+            campaign_id: campaignId,
+            impressions: 1,
+            clicks: 1,
+        })
+        .onConflict((oc) =>
+            oc
+                .columns(['ad_id', 'campaign_id'])
+                .doUpdateSet({
+                    clicks: (eb) => eb('stats.clicks', '+', 1),
+                })
+                .where('stats.ad_id', '=', adId)
+                .where('stats.campaign_id', '=', campaignId)
         )
-        .execute();
+        .execute()
 }
 
 export async function getAdById(adId: number) {
